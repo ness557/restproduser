49ba401397a1716fc46d1939b1df0dba05c12dc0
ness <ness55779@gmail.com>
2018-06-12 10:09:28 +0300

    Initial commit

---
+++ src/main/java/simple/microservices/restproduser/RestproduserApplication.java
@@ -0,0 +1,12 @@
+package simple.microservices.restproduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestproduserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestproduserApplication.class, args);
+    }
+}
